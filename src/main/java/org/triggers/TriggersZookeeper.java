bfe0d33cfae22660fbfbbcd8bd8504103b547041
jewel73 <jewelranar420@gmail.com>
2023-08-30 02:03:22 +0600

    triggers and events code add

--- src/main/java/org/triggers/TriggersZookeeper.java
+++ src/main/java/org/triggers/TriggersZookeeper.java
@@ -1,15 +1,21 @@
 package org.example;
 
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class LeaderSelection implements Watcher {
 
     private ZooKeeper zooKeeper;
     private  static final String ZOOKEEPER_LOCATION = "localhost:2181";
+    private static final String MONITOR_ZNODE = "/test";
     private static final int TIMEOUT=3000;
 
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -36,6 +42,17 @@ public class LeaderSelection implements Watcher {
         }
     }
 
+    public void electedLeader() throws InterruptedException, KeeperException {
+        Stat stat =  zooKeeper.exists(MONITOR_ZNODE, this);
+        if (stat == null) return;
+
+        byte[] data =  zooKeeper.getData(MONITOR_ZNODE, this, stat);
+        List<String> children = zooKeeper.getChildren(MONITOR_ZNODE, this);
+
+        System.out.println("Data from Znode: "+ new String(data) + " children"+ children);
+
+    }
+
     @Override
     public void process(WatchedEvent watchedEvent) {
         switch (watchedEvent.getType()){
@@ -47,6 +64,30 @@ public class LeaderSelection implements Watcher {
                         zooKeeper.notifyAll();
                     }
                 }
+            case NodeCreated:
+                System.out.println("Node created event");
+                break;
+
+            case NodeDeleted:
+                System.out.println("Node has been deleted");
+                break;
+
+            case NodeDataChanged:
+                System.out.println("Data changed in the node");
+                break;
+
+            case NodeChildrenChanged:
+                System.out.println("node has changed.");
+                break;
+
+        }
+
+        try {
+            electedLeader();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (KeeperException e) {
+            throw new RuntimeException(e);
         }
     }
 }
